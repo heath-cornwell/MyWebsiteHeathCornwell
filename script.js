642e10342db67da8af0da1e5a6e48c218e105da4
heath-cornwell <cornwellhec@gmail.com>
2024-08-20 20:16:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        const target = this.getAttribute('href');
+        const offset = 70; 
+
+        if (target === '#top') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            const element = document.querySelector(target);
+            const elementPosition = element.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - offset;
+
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
     });
 });
